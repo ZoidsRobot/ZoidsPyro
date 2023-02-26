792895c2e6a7466f96052b60774b6154f66281aa
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-02-27 03:44:34 +0800

    Create copy.py

---
+++ PyroZen/modules/basic/copy.py
@@ -0,0 +1,39 @@
+# Credits @Eror_404_NF 
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import CMD_HANDLER as cmd
+from PyroZen.helpers.tools import get_arg
+from PyroZen.modules.help import add_command_help
+
+# Define the copy command
+@Client.on_message(filters.command("copy", cmd) & filters.me)
+async def copy(client: Client, message: Message): 
+    # Extract the chat ID from the command link
+    link_parts = message.text.split("/")
+    try:
+        chat_id = link_parts[-2]
+    except (ValueError, IndexError):
+        await message.reply("Invalid link provided.")
+        return
+
+    # Extract the message ID from the link
+    link_parts = message.text.split("/")
+    try:
+        message_id = int(link_parts[-1])
+    except (ValueError, IndexError):
+        await message.reply("Invalid link provided.")
+        return
+
+    # Copy the message to the specified chat
+    try:
+        await client.copy_message(
+            chat_id=chat_id,
+            from_chat_id=message.chat.id,
+            message_id=message_id
+        )
+        await message.reply("Message copied successfully.")
+    except Exception as e:
+        await message.reply(f"Failed to copy message: {e}")

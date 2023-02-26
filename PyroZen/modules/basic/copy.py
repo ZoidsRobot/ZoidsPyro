037f48248f1e6aeef165e9b957a73a20968bb690
Tomi Setiawan <96681219+CyberXGithub@users.noreply.github.com>
2023-02-26 20:44:24 +0700

    Create copy.py

---
+++ PyroZen/modules/basic/copy.py
@@ -0,0 +1,41 @@
+# Credits Tomi Setiawan = @T0M1_X
+
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import CMD_HANDLER as cmd
+from PyroZen.helpers.tools import get_arg
+from PyroZen.modules.help import add_command_help
+
+
+@Client.on_message(filters.command("copy", cmd) & filters.me)
+async def copy_msg(client: Client, message: Message):
+    Tm = await message.reply("`Processing...`")
+    link = get_arg(message)
+    if not link:
+        return await Tm.edit(f"<b><code>{message.text}</code> [link_konten_telegram]</b>")
+    if link.startswith(("https", "t.me")):
+        msg_id = int(link.split("/")[-1])
+        if "t.me/c/" in link:
+            chat = int("-100" + str(link.split("/")[-2]))
+        else:
+            chat = str(link.split("/")[-2])
+        try:
+            get = await client.get_messages(chat, msg_id)
+        except Exception as error:
+            await Tm.edit(error)
+        await get.copy(message.chat.id)
+        return await Tm.delete()
+    else:
+        await Tm.edit("`Berikan link yang valid.`")
+
+
+
+add_command_help(
+    "copy",
+    [
+        [f"{prefix}copy", "nyolong konten orang"],
+    ],
+)

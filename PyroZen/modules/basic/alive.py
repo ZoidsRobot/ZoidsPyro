6dfabee3fddbc17ab11524fba7b348dcb6eb955f
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-03-06 21:52:36 +0800

    Update alive.py

--- PyroZen/modules/basic/alive.py
+++ PyroZen/modules/basic/alive.py
@@ -68,9 +68,9 @@ async def alive(client: Client, message: Message):
             await asyncio.sleep(20),
             await client.delete_messages(chat_id=message.chat.id, message_ids=new_message.id),
         )
-    except BaseException as e:
-         print(f"Error Requests: {e}")
-        return 
+  except BaseException as e:
+        print(f"Error Requests: {e}")
+        return
     await xx.edit_text(man, disable_web_page_preview=True)
         
 @Client.on_message(filters.command("setalivelogo", cmd) & filters.me)
